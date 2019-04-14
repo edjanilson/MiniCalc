e07116cb9000b0743cf23ac64be2518a18f5a3e7
Edjanilson Florencio <edjanilsonflorencioads@gmail.com>
2019-04-14 12:53:04 -0300

    Projeto MiniCalc

---
+++ frmTelaPrincipal.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace MiniCalc
+{
+    partial class frmTelaPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmTelaPrincipal));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.rbAdicao = new System.Windows.Forms.RadioButton();
+            this.rbSubtracao = new System.Windows.Forms.RadioButton();
+            this.rbMultiplicacao = new System.Windows.Forms.RadioButton();
+            this.rbDivisao = new System.Windows.Forms.RadioButton();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.btnApagarValores = new System.Windows.Forms.Button();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tempus Sans ITC", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Informe um número:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tempus Sans ITC", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(187, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Informe outro número:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(16, 160);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(447, 41);
+            this.textBox1.TabIndex = 2;
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNum1.Location = new System.Drawing.Point(213, 25);
+            this.txtNum1.MaxLength = 10;
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(151, 26);
+            this.txtNum1.TabIndex = 0;
+            this.txtNum1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNum2.Location = new System.Drawing.Point(213, 72);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(151, 26);
+            this.txtNum2.TabIndex = 1;
+            this.txtNum2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tempus Sans ITC", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(134, 133);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(211, 24);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Escolha uma operação:";
+            // 
+            // rbAdicao
+            // 
+            this.rbAdicao.AutoSize = true;
+            this.rbAdicao.BackColor = System.Drawing.Color.White;
+            this.rbAdicao.Font = new System.Drawing.Font("Tempus Sans ITC", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbAdicao.Location = new System.Drawing.Point(27, 167);
+            this.rbAdicao.Name = "rbAdicao";
+            this.rbAdicao.Size = new System.Drawing.Size(80, 24);
+            this.rbAdicao.TabIndex = 2;
+            this.rbAdicao.TabStop = true;
+            this.rbAdicao.Text = "Adição";
+            this.rbAdicao.UseVisualStyleBackColor = false;
+            // 
+            // rbSubtracao
+            // 
+            this.rbSubtracao.AutoSize = true;
+            this.rbSubtracao.BackColor = System.Drawing.Color.White;
+            this.rbSubtracao.Font = new System.Drawing.Font("Tempus Sans ITC", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbSubtracao.Location = new System.Drawing.Point(113, 167);
+            this.rbSubtracao.Name = "rbSubtracao";
+            this.rbSubtracao.Size = new System.Drawing.Size(101, 24);
+            this.rbSubtracao.TabIndex = 3;
+            this.rbSubtracao.TabStop = true;
+            this.rbSubtracao.Text = "Subtração";
+            this.rbSubtracao.UseVisualStyleBackColor = false;
+            // 
+            // rbMultiplicacao
+            // 
+            this.rbMultiplicacao.AutoSize = true;
+            this.rbMultiplicacao.BackColor = System.Drawing.Color.White;
+            this.rbMultiplicacao.Font = new System.Drawing.Font("Tempus Sans ITC", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbMultiplicacao.Location = new System.Drawing.Point(220, 167);
+            this.rbMultiplicacao.Name = "rbMultiplicacao";
+            this.rbMultiplicacao.Size = new System.Drawing.Size(125, 24);
+            this.rbMultiplicacao.TabIndex = 4;
+            this.rbMultiplicacao.TabStop = true;
+            this.rbMultiplicacao.Text = "Multplicação";
+            this.rbMultiplicacao.UseVisualStyleBackColor = false;
+            // 
+            // rbDivisao
+            // 
+            this.rbDivisao.AutoSize = true;
+            this.rbDivisao.BackColor = System.Drawing.Color.White;
+            this.rbDivisao.Font = new System.Drawing.Font("Tempus Sans ITC", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbDivisao.Location = new System.Drawing.Point(362, 167);
+            this.rbDivisao.Name = "rbDivisao";
+            this.rbDivisao.Size = new System.Drawing.Size(81, 24);
+            this.rbDivisao.TabIndex = 5;
+            this.rbDivisao.TabStop = true;
+            this.rbDivisao.Text = "Divisão";
+            this.rbDivisao.UseVisualStyleBackColor = false;
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResultado.Location = new System.Drawing.Point(158, 256);
+            this.txtResultado.MaxLength = 10;
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.ReadOnly = true;
+            this.txtResultado.Size = new System.Drawing.Size(154, 26);
+            this.txtResultado.TabIndex = 6;
+            this.txtResultado.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tempus Sans ITC", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(186, 229);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 24);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Resultado";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::MiniCalc.Properties.Resources.iconfinder_draw_08_7255581;
+            this.pictureBox1.Location = new System.Drawing.Point(370, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(35, 32);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 19;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFechar.Image = global::MiniCalc.Properties.Resources.iconfinder_Close_1891023;
+            this.btnFechar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnFechar.Location = new System.Drawing.Point(317, 313);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(107, 58);
+            this.btnFechar.TabIndex = 18;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnApagarValores
+            // 
+            this.btnApagarValores.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnApagarValores.Image = global::MiniCalc.Properties.Resources.iconfinder_draw_08_725558;
+            this.btnApagarValores.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnApagarValores.Location = new System.Drawing.Point(181, 313);
+            this.btnApagarValores.Name = "btnApagarValores";
+            this.btnApagarValores.Size = new System.Drawing.Size(111, 58);
+            this.btnApagarValores.TabIndex = 17;
+            this.btnApagarValores.Text = "Apagar \r\nvalores";
+            this.btnApagarValores.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnApagarValores.UseVisualStyleBackColor = true;
+            this.btnApagarValores.Click += new System.EventHandler(this.btnApagarValores_Click);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Image = global::MiniCalc.Properties.Resources.iconfinder_YouTube_191274;
+            this.btnCalcular.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCalcular.Location = new System.Drawing.Point(60, 313);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(95, 58);
+            this.btnCalcular.TabIndex = 7;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::MiniCalc.Properties.Resources.iconfinder_draw_08_7255581;
+            this.pictureBox2.Location = new System.Drawing.Point(370, 72);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(35, 32);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox2.TabIndex = 20;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // frmTelaPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(489, 383);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.btnApagarValores);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.rbDivisao);
+            this.Controls.Add(this.rbMultiplicacao);
+            this.Controls.Add(this.rbSubtracao);
+            this.Controls.Add(this.rbAdicao);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmTelaPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MiniCalc";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton rbAdicao;
+        private System.Windows.Forms.RadioButton rbSubtracao;
+        private System.Windows.Forms.RadioButton rbMultiplicacao;
+        private System.Windows.Forms.RadioButton rbDivisao;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnApagarValores;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ frmTelaPrincipal.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiniCalc
+{
+    public partial class frmTelaPrincipal : Form
+    {
+        public frmTelaPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            //Declarando as variáveis.
+            double num1, num2, adicao, subtracao, multiplicacao, divisao;
+
+            if (txtNum1.Text == "" || txtNum2.Text == "")
+            {
+                MessageBox.Show("Ocorreu algo errado, verifique as informações e tente novamente.", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                try
+                {
+
+
+
+                    //Inicializando as variáveis.
+                    num1 = double.Parse(txtNum1.Text);
+                    num2 = double.Parse(txtNum2.Text);
+
+                    //Se a opção Adição for marcada.
+                    if (rbAdicao.Checked == true)
+                    {
+                        //Então será realizada uma adição dos números inseridos.
+                        adicao = num1 + num2;
+                        //E mostrará o resultado.
+                        txtResultado.Text = adicao.ToString();
+                    }
+
+                    //Senão se a opção Subtração for marcada.
+                    else
+                        if (rbSubtracao.Checked == true)
+                    {
+                        //Então será realizada uma subtração.
+                        subtracao = num1 - num2;
+                        //E mostrará o resultado.
+                        txtResultado.Text = subtracao.ToString();
+                    }
+
+                    //Senão se a opção Multiplicação for marcada.
+                    else
+                        if (rbMultiplicacao.Checked == true)
+                    {
+                        //Então será realizada uma Multiplicação.
+                        multiplicacao = num1 * num2;
+                        //E mostrará o resultado.
+                        txtResultado.Text = multiplicacao.ToString();
+                    }
+
+                    //Senão se a opção Divisão for marcada.
+                    else
+                        if (rbDivisao.Checked == true)
+                    {
+                        //Então será realizada uma Divisão.
+                        divisao = num1 / num2;
+                        //E mostrará o resultado.
+                        txtResultado.Text = divisao.ToString();
+                    }
+                }catch(FormatException)
+                {
+                    MessageBox.Show("Você dever digitar apenas números, tente novamente.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Aparece uma mensagem perguntando se realmente desejamos fechar o programa.
+            if(MessageBox.Show("Tem certeza que deseja fechar o programa?","Atenção",MessageBoxButtons.YesNo,MessageBoxIcon.Question)==DialogResult.Yes)
+            {
+                //Caso a resposta seja sim, o programa irá fechar.
+                this.Close();
+            }
+        }
+
+        private void btnApagarValores_Click(object sender, EventArgs e)
+        {
+            //Apaga todos os valores numéricos do formulário.
+            txtNum1.Clear();
+            txtNum2.Clear();
+            txtResultado.Clear();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            //Apga o conteúdo do TextBox 1.
+            txtNum1.Clear();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            //Apaga o conteúdo do TextBox 2
+            txtNum2.Clear();
+        }
+    }
+}
